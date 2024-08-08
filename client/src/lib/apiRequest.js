be27c5b899997958be1016c1b6b8289834793872
Muhammad Arsalan <arsalan.online24@gmail.com>
2024-08-08 21:06:30 +0500

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    // baseURL: "http://localhost:8800/api",
+        baseURL: "https://real-estate-full-stack-platform-1.onrender.com/api",
   withCredentials: true,
 });
 
